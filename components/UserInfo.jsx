1642dcf3120d9146de6c5da676f4a5c70d294aef
CHUNGTU-120837\Chung Tú <nguyentutsq@gmail.com>
2023-08-04 23:23:33 +0700

    first commit

---
+++ components/UserInfo.jsx
@@ -0,0 +1,28 @@
+"use client"
+import React from 'react'
+import SignInButton from './SignInButton'
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+
+const UserInfo = () => {
+    const { status, data: session } = useSession()
+    if(status === 'authenticated'){
+        return (
+            <div className='shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-200'>
+                <Image className='rounded-full' src={session?.user?.image} width={60} height={60} alt="user img"/>
+                <div>
+                    Name: <span className='font-bold'>{session?.user?.name}</span>
+                </div>
+                <div>
+                    Email: <span className='font-bold'>{session?.user?.email}</span>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <SignInButton />
+        )
+    }
+}
+
+export default UserInfo
