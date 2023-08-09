05164d308b679ee8f9fa96fea971d3c5e9e0f8cc
CHUNGTU-120837\Chung Tú <nguyentutsq@gmail.com>
2023-08-10 04:45:31 +0700

    commmit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,15 @@
 import Link from 'next/link'
 import React from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 const Navbar = () => {
     const { status } = useSession()
     return (
         <div className='p-4 flex justify-between items-center shadow-md'>
-            <Link className='font-bold text-lg text-blue-700' href={'/'}>Next Auth</Link>
+            <Link className='font-bold text-lg text-blue-700' href={'/'}>
+                <Image src="https://img.icons8.com/color-pixels/32/experimental-duck-color-pixels.png" width={50} height={50} alt="logo duck"/>
+            </Link>
             {
                 status === 'authenticated' ? (
                     <button onClick={() => signOut()} className='bg-slate-900 text-white px-6 py-2 rounded-md'>Đăng xuất</button>
